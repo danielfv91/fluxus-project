da91bb053d0a9d4cb01169bd4a5b194d45f4f191
Daniel Vasconcelos <danielfelipe91@gmail.com>
2025-03-28 19:51:35 -0300

    test: adiciona testes para listagem de transações com filtros de data
    - Testa ListTransactionsHandler com mock de repositório, usuário e mapper
    - Testa validação de intervalo de datas no ListTransactionsQueryValidator
    - Utiliza FluentAssertions, NSubstitute e Bogus para geração de dados
    - Organiza dados de teste em TestData/ListTransactionsHandlerTestData

---
+++ tests/Fluxus.Unit/Application/Features/Transactions/ListTransactions/ListTransactionsQueryValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using Fluxus.Application.Features.Transactions.ListTransactions;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Fluxus.UnitTests.Application.Features.Transactions.ListTransactions
+{
+    public class ListTransactionsQueryValidatorTests
+    {
+        private readonly ListTransactionsQueryValidator _validator = new();
+
+        [Fact]
+        public void Should_Pass_When_Dates_Are_Null()
+        {
+            // Arrange
+            var query = new ListTransactionsQuery();
+
+            // Act
+            var result = _validator.TestValidate(query);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Should_Pass_When_DateFrom_Is_Less_Than_Or_Equal_To_DateTo()
+        {
+            var query = new ListTransactionsQuery
+            {
+                DateFrom = new DateTime(2025, 03, 01),
+                DateTo = new DateTime(2025, 03, 31)
+            };
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Should_Fail_When_DateFrom_Is_Greater_Than_DateTo()
+        {
+            var query = new ListTransactionsQuery
+            {
+                DateFrom = new DateTime(2025, 04, 01),
+                DateTo = new DateTime(2025, 03, 01)
+            };
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(q => q.DateFrom)
+                .WithErrorMessage("A data inicial deve ser menor ou igual à data final.");
+        }
+    }
+}
