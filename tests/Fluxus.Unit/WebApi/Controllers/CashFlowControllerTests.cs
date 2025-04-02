517ccd9aac114999e4b710294ef7734b7d03de54
Daniel Vasconcelos <danielfelipe91@gmail.com>
2025-04-02 03:16:29 -0300

    test(webapi): adiciona testes unitários para todos os controllers

---
+++ tests/Fluxus.Unit/WebApi/Controllers/CashFlowControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Fluxus.WebApi.Features.CashFlows;
+using Fluxus.Application.Features.CashFlows.DailyCashFlow;
+using Fluxus.WebApi.Features.CashFlows.DailyCashFlow;
+using System.Security.Claims;
+using Fluxus.WebApi.Common;
+
+namespace Fluxus.Unit.WebApi.Controllers;
+
+public class CashFlowControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly CashFlowController _controller;
+
+    public CashFlowControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _mapperMock = new Mock<IMapper>();
+        _controller = new CashFlowController(_mediatorMock.Object, _mapperMock.Object);
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+        }));
+
+        _controller.ControllerContext = new ControllerContext()
+        {
+            HttpContext = new DefaultHttpContext() { User = user }
+        };
+    }
+
+    [Fact]
+    public async Task GetDailyCashFlow_ShouldReturnOk_WithListOfResponses()
+    {
+        // Arrange
+        var request = new DailyCashFlowRequest
+        {
+            DateFrom = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+            DateTo = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        var query = new DailyCashFlowQuery();
+        var result = new List<DailyCashFlowResult> { new() };
+        var response = new List<DailyCashFlowResponse> { new() };
+
+        _mapperMock.Setup(m => m.Map<DailyCashFlowQuery>(request)).Returns(query);
+        _mediatorMock.Setup(m => m.Send(query, default)).ReturnsAsync(result);
+        _mapperMock.Setup(m => m.Map<List<DailyCashFlowResponse>>(result)).Returns(response);
+
+        // Act
+        var actionResult = await _controller.GetDailyCashFlow(request, default);
+        var okResult = actionResult as OkObjectResult;
+
+        // Assert
+        okResult.Should().NotBeNull();
+        okResult!.Value.Should().BeOfType<ApiResponseWithData<List<DailyCashFlowResponse>>>();
+        var data = okResult.Value as ApiResponseWithData<List<DailyCashFlowResponse>>;
+        data!.Success.Should().BeTrue();
+        data.Data.Should().BeEquivalentTo(response);
+    }
+}
---
+++ tests/Fluxus.Unit/WebApi/Controllers/CashFlowReportControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Fluxus.WebApi.Features.CashFlows.Reports;
+using Fluxus.Application.Features.CashFlows.DailyCashFlow.Reports.GenerateDailyCashFlowReport;
+using Fluxus.Common.Security.Interfaces;
+using Fluxus.Common.Reporting.Constants;
+using Fluxus.WebApi.Features.CashFlows.DailyCashFlow.Reports;
+
+namespace Fluxus.Unit.WebApi.Controllers;
+
+public class CashFlowReportControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly Mock<IAuthenticatedUser> _userMock;
+    private readonly CashFlowReportController _controller;
+
+    public CashFlowReportControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _userMock = new Mock<IAuthenticatedUser>();
+        _controller = new CashFlowReportController(_mediatorMock.Object, _userMock.Object);
+    }
+
+    [Fact]
+    public async Task Export_ShouldReturnFile_WhenSuccess()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var request = new ExportDailyCashFlowReportRequest
+        {
+            DateFrom = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+            DateTo = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        var result = new GenerateDailyCashFlowReportResult
+        {
+            FileContent = new byte[] { 1, 2, 3 },
+            ContentType = "application/pdf",
+            FileName = "relatorio.pdf"
+        };
+
+        _userMock.Setup(x => x.Id).Returns(userId);
+        _mediatorMock.Setup(m => m.Send(It.IsAny<GenerateDailyCashFlowReportQuery>(), default))
+                     .ReturnsAsync(result);
+
+        // Act
+        var response = await _controller.Export(request, default);
+
+        // Assert
+        response.Should().BeOfType<FileContentResult>();
+        var file = response as FileContentResult;
+        file!.ContentType.Should().Be("application/pdf");
+        file.FileDownloadName.Should().Be("relatorio.pdf");
+        file.FileContents.Should().BeEquivalentTo(result.FileContent);
+    }
+
+    [Fact]
+    public async Task Export_ShouldReturn504_WhenTimeout()
+    {
+        // Arrange
+        _userMock.Setup(x => x.Id).Returns(Guid.NewGuid());
+        _mediatorMock.Setup(m => m.Send(It.IsAny<GenerateDailyCashFlowReportQuery>(), default))
+                     .ThrowsAsync(new TaskCanceledException());
+
+        var request = new ExportDailyCashFlowReportRequest
+        {
+            DateFrom = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+            DateTo = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        // Act
+        var response = await _controller.Export(request, default) as ObjectResult;
+
+        // Assert
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(504);
+        response.Value.Should().BeEquivalentTo(new
+        {
+            success = false,
+            message = ReportMessages.TimeoutError
+        });
+    }
+
+    [Fact]
+    public async Task Export_ShouldReturn500_WhenUnhandledError()
+    {
+        // Arrange
+        _userMock.Setup(x => x.Id).Returns(Guid.NewGuid());
+        _mediatorMock.Setup(m => m.Send(It.IsAny<GenerateDailyCashFlowReportQuery>(), default))
+                     .ThrowsAsync(new Exception("Algo deu errado"));
+
+        var request = new ExportDailyCashFlowReportRequest
+        {
+            DateFrom = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+            DateTo = DateOnly.FromDateTime(DateTime.Today)
+        };
+
+        // Act
+        var response = await _controller.Export(request, default) as ObjectResult;
+
+        // Assert
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(500);
+        response.Value.Should().BeEquivalentTo(new
+        {
+            success = false,
+            message = ReportMessages.UnexpectedError,
+            detail = "Algo deu errado"
+        });
+    }
+}
