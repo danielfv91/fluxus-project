211b04dc0d5e63c790a3168e3e764ca9ef4ccd49
Daniel Vasconcelos <danielfelipe91@gmail.com>
2025-04-02 00:08:37 -0300

    test: adiciona testes unitários para geração do relatório diário em PDF

---
+++ tests/Fluxus.Unit/Application/Features/CashFlows/DailyCashFlow/Reports/GenerateDailyCashFlowReport/GenerateDailyCashFlowReportProfileTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Fluxus.Application.Features.CashFlows.DailyCashFlow.Reports.GenerateDailyCashFlowReport;
+using Fluxus.Common.Reporting.Models;
+using Fluxus.UnitTests.Application.TestData;
+using FluentAssertions;
+using Xunit;
+
+namespace Fluxus.UnitTests.Application.Features.CashFlows.Reports.GenerateDailyCashFlowReport
+{
+    public class GenerateDailyCashFlowReportProfileTests
+    {
+        private readonly IMapper _mapper;
+
+        public GenerateDailyCashFlowReportProfileTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<GenerateDailyCashFlowReportProfile>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public void Should_Map_DailyConsolidation_To_ReportModel()
+        {
+            // Arrange
+            var consolidations = GenerateDailyCashFlowReportTestData.GenerateConsolidations();
+
+            // Act
+            var result = _mapper.Map<List<DailyCashFlowReportModel>>(consolidations);
+
+            // Assert
+            result.Should().HaveCount(consolidations.Count);
+
+            for (int i = 0; i < consolidations.Count; i++)
+            {
+                result[i].Date.Should().Be(consolidations[i].Date);
+                result[i].TotalCredits.Should().Be(consolidations[i].TotalCredits);
+                result[i].TotalDebits.Should().Be(consolidations[i].TotalDebits);
+            }
+        }
+    }
+}
---
+++ tests/Fluxus.Unit/Application/TestData/GenerateDailyCashFlowReportTestData.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using Fluxus.Application.Features.CashFlows.DailyCashFlow.Reports.GenerateDailyCashFlowReport;
+using Fluxus.Common.Reporting.Models;
+using Fluxus.Domain.Entities;
+
+namespace Fluxus.UnitTests.Application.TestData
+{
+    public static class GenerateDailyCashFlowReportTestData
+    {
+        public static readonly Guid UserId = Guid.NewGuid();
+
+        public static List<DailyConsolidation> GenerateConsolidations()
+        {
+            return new List<DailyConsolidation>
+            {
+                new() { Id = Guid.NewGuid(), UserId = UserId, Date = new DateOnly(2025, 03, 01), TotalCredits = 100, TotalDebits = 30 },
+                new() { Id = Guid.NewGuid(), UserId = UserId, Date = new DateOnly(2025, 03, 02), TotalCredits = 150, TotalDebits = 50 },
+                new() { Id = Guid.NewGuid(), UserId = UserId, Date = new DateOnly(2025, 03, 03), TotalCredits = 80, TotalDebits = 60 }
+            };
+        }
+
+        public static GenerateDailyCashFlowReportQuery CreateQueryWithRange()
+        {
+            return new GenerateDailyCashFlowReportQuery
+            {
+                UserId = UserId,
+                DateFrom = new DateOnly(2025, 03, 01),
+                DateTo = new DateOnly(2025, 03, 03)
+            };
+        }
+
+        public static List<DailyCashFlowReportModel> GenerateMappedReportModels()
+        {
+            var faker = new Faker<DailyCashFlowReportModel>()
+                .RuleFor(m => m.Date, f => new DateOnly(2025, 03, f.Random.Int(1, 3)))
+                .RuleFor(m => m.TotalCredits, f => f.Finance.Amount(50, 200))
+                .RuleFor(m => m.TotalDebits, f => f.Finance.Amount(10, 100))
+                .RuleFor(m => m.AccumulatedBalance, 0);
+
+            return faker.Generate(3).OrderBy(x => x.Date).ToList();
+        }
+    }
+}
